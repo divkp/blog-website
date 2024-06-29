1f1e5f8d0d72a92d55d68f3ed89d7e9903a3c8d9
Divyam <116710009+divkp@users.noreply.github.com>
2024-06-29 18:28:39 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,9 +8,20 @@ import dotenv from "dotenv";
 const app = express();
 
 // Configure CORS
-app.use(cors(
-  {origin:["https://blog-website-ette.vercel.app"]
-  }));
+const allowedOrigins = ['https://blog-website-ette.vercel.app'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+// Use the CORS middleware
+app.use(cors(corsOptions));
 dotenv.config();
 
 app.use(bodyParser.json({ extended: true }));
